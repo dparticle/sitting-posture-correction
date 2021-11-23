bfbb147c8af89819f91d651f53a2678c5d8ac6a3
dparticle <dparticle@163.com>
2021-11-23 22:02:01 +0800

    refactor: 函数式编程

--- correction-h5/src/pages/index/index.tsx
+++ correction-h5/src/pages/index/index.tsx
@@ -1,68 +1,30 @@
-import { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector, useStore } from 'react-redux'
 import { View, Button, Text } from '@tarojs/components'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
 
 import './index.less'
 
-type PageStateProps = {
-  counter: {
-    num: number
-  }
-}
+const Index = (props: any) => {
+  const dispatch = useDispatch()
+  const {num} = useSelector(state => state.counter) // 忽略 IDE 报错
+  console.log('num', num);
 
-type PageDispatchProps = {
-  add: () => void
-  dec: () => void
-  asyncAdd: () => any
-}
 
-type PageOwnProps = {}
-
-type PageState = {}
-
-type IProps = PageStateProps & PageDispatchProps & PageOwnProps
-
-interface Index {
-  props: IProps;
-}
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
+  useEffect(() => {
     dispatch(asyncAdd())
-  }
-}))
-class Index extends Component {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
-  }
+  }, [])
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
+  return (
       <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
+        <Button className='add_btn' onClick={() => dispatch(add())}>+</Button>
+        <Button className='dec_btn' onClick={() => dispatch(minus())}>-</Button>
+        <Button className='dec_btn' onClick={() => dispatch(asyncAdd())}>async</Button>
+        <View><Text>{num}</Text></View>
         <View><Text>Hello, World</Text></View>
       </View>
     )
-  }
 }
 
 export default Index
