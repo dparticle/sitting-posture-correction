280523a50a854b9f222062edba6ffa56ec1879f9
dparticle <dparticle@163.com>
2021-11-24 15:03:16 +0800

    feat: 首页界面设计

--- correction-h5/src/pages/index/index.tsx
+++ correction-h5/src/pages/index/index.tsx
@@ -1,30 +1,96 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector, useStore } from 'react-redux'
-import { View, Button, Text } from '@tarojs/components'
-
-import { add, minus, asyncAdd } from '../../actions/counter'
+import React, { useEffect, useState } from 'react'
+import { View, Text } from '@tarojs/components'
+import { Circle, Icon } from "@antmjs/vantui"
 
 import './index.less'
 
 const Index = (props: any) => {
-  const dispatch = useDispatch()
-  const {num} = useSelector(state => state.counter) // 忽略 IDE 报错
-  console.log('num', num);
+  const [ play, setPlay ] = useState(false)
 
+  const onPlay = () => {
+    setPlay(true)
+  }
+
+  const onStop = () => {
+    setPlay(false)
+  }
 
   useEffect(() => {
-    dispatch(asyncAdd())
   }, [])
 
   return (
-      <View className='index'>
-        <Button className='add_btn' onClick={() => dispatch(add())}>+</Button>
-        <Button className='dec_btn' onClick={() => dispatch(minus())}>-</Button>
-        <Button className='dec_btn' onClick={() => dispatch(asyncAdd())}>async</Button>
-        <View><Text>{num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+    <View className='index'>
+      <View className='stat'>
+        <Icon
+          className='btn-stat position-center'
+          name='chart-trending-o'
+          size='20px'
+          color='#999'
+        />
       </View>
-    )
+      <View className='countdown'>
+        <Circle
+          className='circle'
+          value={ 100 }
+          size={ 200 }
+          strokeWidth={ 10 }
+          color={ {
+            '0%': '#7f7fd5',
+            '50%': '#86a8e7',
+            '100%': '#91eae4'
+          } }
+          clockwise={ false }
+        >
+          <Text className='time position-center'>
+          20:00
+          </Text>
+        </Circle>
+      </View>
+      <View className='control'>
+        {play ? (
+          // 开始按钮组
+          <>
+            <View className='btn-control'>
+              <Icon
+                className='position-center'
+                name='pause'
+                size='32px'
+                color='#3498db'
+              />
+            </View>
+            <View className='btn-control' onClick={onStop}>
+              <Icon
+                className='position-center'
+                name='stop'
+                size='44px'
+                color='#e74c3c'
+              />
+            </View>
+          </>
+        ) : (
+          // 未开始按钮组
+          <>
+            <View className='btn-control' onClick={onPlay}>
+              <Icon
+                className='position-center'
+                name='play'
+                size='32px'
+                color='#2ecc71'
+              />
+            </View>
+            <View className='btn-control'>
+              <Icon
+                className='position-center'
+                name='setting'
+                size='28px'
+                color='#34495e'
+              />
+            </View>
+          </>
+        )}
+      </View>
+    </View>
+  )
 }
 
 export default Index
