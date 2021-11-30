0ad5b6e2aa3aa88e0b647fcd94ca4932fb94451c
dparticle <dparticle@163.com>
2021-11-30 13:13:16 +0800

    feat: esp8266 oled 显示

--- correction-esp8266/correction-esp8266.ino
+++ correction-esp8266/correction-esp8266.ino
@@ -1,3 +1,4 @@
+#include <U8g2lib.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
@@ -16,11 +17,14 @@ const char* topic_sub = "correction/h5";
 
 WiFiClient esp_client;
 PubSubClient client(esp_client);
-StaticJsonDocument<200> doc;
 String client_id;
+StaticJsonDocument<200> doc;
+// 创建 u8g2
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/14, /* data=*/2, /* reset=*/U8X8_PIN_NONE);
 float cm;
 unsigned long now, keep;  // 用于状态更改时间判断、记录正常坐姿维持开始时间
 bool flag = false, start = false;
+int nearest = 7, longest = 14, un = 30;
 
 // 连接 Wi-Fi
 void setup_wifi() {
@@ -44,6 +48,23 @@ void setup_wifi() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+void u8g2Status(char* status) {
+  u8g2.clearBuffer();
+  // u8g2 设置字体集
+  u8g2.setFont(u8g2_font_wqy14_t_gb2312);
+  u8g2.setCursor(0, 14);
+  u8g2.print("健康专注");
+  u8g2.setFont(u8g2_font_wqy12_t_gb2312);
+  u8g2.setCursor(0, 26);
+  u8g2.print("topic_sub:");
+  u8g2.setCursor(0, 38);
+  u8g2.print(topic_sub);
+  u8g2.setCursor(0, 52);
+  u8g2.print("状态：");
+  u8g2.print(status);
+  u8g2.sendBuffer();
 }
 
 void pub_status(char* status) {
@@ -87,12 +108,23 @@ void callback(char* topic, byte* payload, unsigned int length) {
   } else if (p == "start") {
     start = true;
     flag = false; // 初始化 flag
-  } else if (p == "pause") {
+  } else if (p == "recover") {
     start = true;
+    u8g2Status("专注中...");
+  } else if (p == "pause") {
+    start = false;
+    u8g2Status("暂停...");
   } else if (p == "stop") {
     start = false;
+    if (doc["remark"] == nullptr) {
+      u8g2Status("结束！");
+    } else {
+      u8g2Status("专注失败！");
+      // u8g2Status(doc["remark"]);
+    }
   } else if (p == "timeout") {
-    if (!flag) flag = true;
+    if (!flag) start = false;
+    u8g2Status("待机...");
   }
 }
 
@@ -139,10 +171,11 @@ void normal_handle() {
   // 2s 内稳定处于正常距离
   if (!flag) {
     now = millis();
+    // u8g2Status("准备开始..."); // 耗时任务
     while (millis() - now < START_DURATION) {
       cm = get_distance();
       // 过滤异常
-      if (cm < 30 && !(cm >= 7 && cm <= 13)) {
+      if (cm < un && !(cm > nearest && cm < longest)) {
         break;
       }
       // 提示
@@ -153,6 +186,8 @@ void normal_handle() {
       Serial.print("开始专注！");
       flag = true;
       keep = millis();
+      doc.clear(); // 清除上次 doc
+      u8g2Status("专注中...");
       pub_status("start");
     }
   }
@@ -167,7 +202,7 @@ void near_handle() {
     now = millis();
     while (millis() - now < ADJUST_DURATION) {
       cm = get_distance();
-      if (cm >= 7 && cm <= 13) {
+      if (cm > nearest && cm < longest) {
         break;
       }
       // 提示
@@ -178,6 +213,7 @@ void near_handle() {
     }
     if (millis() - now >= ADJUST_DURATION) {
       flag = false;
+      start = false;
       pub_fail("远于正常距离");
     }
   }
@@ -192,7 +228,7 @@ void far_handle() {
     now = millis();
     while (millis() - now < ADJUST_DURATION) {
       cm = get_distance();
-      if (cm >= 7 && cm <= 13) {
+      if (cm > nearest && cm < longest) {
         break;
       }
       // 提示
@@ -203,6 +239,7 @@ void far_handle() {
     }
     if (millis() - now >= ADJUST_DURATION) {
       flag = false;
+      start = false;
       pub_fail("近于正常距离");
     }
   }
@@ -215,7 +252,7 @@ void exception_handle() {
     now = millis();
     while (millis() - now < EXCEPTION_DURATION) {
       cm = get_distance();
-      if (cm < 30) {
+      if (cm < un) {
         break;
       }
       Serial.print("数据异常");
@@ -223,8 +260,9 @@ void exception_handle() {
     }
     if (millis() - now >= EXCEPTION_DURATION) {
       Serial.println("数据异常");
-      pub_fail("数据异常");
       flag = false;
+      start = false;
+      pub_fail("数据异常");
     }
   }
 }
@@ -234,6 +272,14 @@ void setup() {
   // 设置 HC-SR04 引脚
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
+
+  // 开启 u8g2，内置 clearDisplay
+  u8g2.begin();
+  u8g2.enableUTF8Print();
+  // u8g2 设置字体方向
+  u8g2.setFontDirection(0);
+  u8g2Status("待机...");
+
   setup_wifi();
   client.setServer(mqtt_server, 11922);
   client.setCallback(callback);
@@ -249,10 +295,10 @@ void loop() {
   if (start) {
     cm = get_distance();
 
-    if (cm < 30) {
-      if (cm >= 7 && cm <= 13) {
+    if (cm < un) {
+      if (cm > nearest && cm < longest) {
         normal_handle();
-      } else if (cm > 13) {
+      } else if (cm > longest) {
         near_handle();
       } else {
         far_handle();
